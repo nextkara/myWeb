0f66fd666201f8f864d384a791107ce7705a29cc
nextkara <nextkara@hotmail.com>
2018-06-07 23:44:06 +0800

    改进了模板和view.py

--- main/views.py
+++ main/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
-
+from .models import menu
 # Create your views here.
 def testWeb(Request):
-    return render(Request,'index.html')
+    listOfMenu = []
+    listMenu = menu.objects.all()
+    for x in listMenu:
+        listOfMenu.append({'menu_link':x.menuLink, 'menu_img':x.menuImg, 'menu_name':x.menuName})
+    return render(Request, 'monitor.html', {'info_dict': listOfMenu, 'path_request': 'monitor', 'name':'控制台'})
 
 
 def testHelp(Request):
-    return render(Request, 'help.html')
+    listOfMenu = []
+    listMenu = menu.objects.all()
+    for x in listMenu:
+        listOfMenu.append({'menu_link': x.menuLink, 'menu_img': x.menuImg, 'menu_name': x.menuName})
+    return render(Request, 'help.html', {'info_dict': listOfMenu, 'path_request': 'monitor', 'name':'关于我们'})
 
 def testWorklist(Request):
-    return render(Request, 'worklist.html')
+    listOfMenu = []
+    listMenu = menu.objects.all()
+    for x in listMenu:
+        listOfMenu.append({'menu_link': x.menuLink, 'menu_img': x.menuImg, 'menu_name': x.menuName})
+    return render(Request, 'worklist.html', {'info_dict': listOfMenu, 'path_request': 'monitor', 'name':'作业系统'})
 
 def testUpload(Request):
-    return render(Request, 'upload.html')
+    listOfMenu = []
+    listMenu = menu.objects.all()
+    for x in listMenu:
+        listOfMenu.append({'menu_link': x.menuLink, 'menu_img': x.menuImg, 'menu_name': x.menuName})
+    return render(Request, 'upload.html', {'info_dict': listOfMenu, 'path_request': 'monitor', 'name':'文件上传'})
 
 def testDownload(Request):
-    return render(Request, 'download.html')
+    listOfMenu = []
+    listMenu = menu.objects.all()
+    for x in listMenu:
+        listOfMenu.append({'menu_link': x.menuLink, 'menu_img': x.menuImg, 'menu_name': x.menuName})
+    return render(Request, 'download.html', {'info_dict': listOfMenu, 'path_request': 'monitor', 'name':'文件下载'})
