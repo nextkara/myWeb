85ee06e665abe09af4a3a8fb2cf98b4d15af7e68
nextkara <nextkara@hotmail.com>
2018-06-08 00:01:30 +0800

    更新了启动脚本

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-clear
-python3 manage.py runserver
+which python3 &> /dev/null
+if [[ $? -eq 0 ]];then
+    PYTHON3BIN=`which python3`
+    nohup $PYTHON3BIN manage.py runserver &> output.log
+    [[ $? -eq 0 ]] && echo "start successful"
+elif [[ -f /data/Android/new_script/test/python3/bin/python3 ]];then
+    nohup /data/Android/new_script/test/python3/bin/python3 manager.py runserver &> output.log
+    [[ $? -eq 0 ]] && echo "start successful"
+else
+    echo "no python3, quit now"
+    exit 1
+fi
